35faf06d5b034f90ab19ebe22d38ccf17c0fbbde
garridodsnshop <49696167+garridodsnshop@users.noreply.github.com>
2019-04-16 17:12:20 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Teste Garrido');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
 });
